6dce0eba022538a6902f89973a7620b072d2f277
matt-oconnell <mattoconnell408@gmail.com>
2016-09-06 14:30:55 -0400

    changing it up

--- src/components/app.js
+++ src/components/app.js
@@ -1,53 +1,13 @@
-import React, { PropTypes } from 'react'
-import _ from 'lodash'
-import YTSearch from 'youtube-api-search'
+import React, { Component } from 'react'
 
-import SearchBar from './search_bar'
-import VideoList from './video_list'
-import VideoDetail from './video_detail'
+import BookList from '../containers/book-list'
 
-const API_KEY = 'AIzaSyD40C8AxXE1pS0QIHSusiE_Q8sT-P7oK-8';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      videos: [],
-      selectedVideo: null
-    }
-
-    this.videoSearch('surfboards')
+class App extends Component {
+  render () {
+    <div>
+      <BookList />
+    </div>
   }
-
-  videoSearch(term) {
-    YTSearch({
-        key: API_KEY,
-        term: term
-      },
-      videos => this.setState({
-        selectedVideo: videos[0],
-        videos
-      })
-    )
-  }
-
-  render() {
-    const videoSearch = _.debounce(term => { this.videoSearch(term) }, 500)
-
-    return (
-      <div>
-        Search:
-        <SearchBar onSearchTermChange={videoSearch} />
-        <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList
-          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-          videos={this.state.videos} />
-      </div>
-    )
-  }
-
-
 }
 
 export default App;
